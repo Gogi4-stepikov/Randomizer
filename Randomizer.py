c5421ef250516f9ef35a31135f631e64d6415519
Gogi4-stepikov <gogieathotdoga@gmail.com>
2025-04-16 20:46:15 +0300

    Update Randomizer.py

--- Randomizer.py
+++ Randomizer.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask, send_from_directory, render_template
+import os
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'icon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 @app.route('/')
-def index():
-    with open('Randomizer_Web.html', 'r', encoding='utf-8') as file:
-        return file.read()
+def home():
+    return render_template('Randomizer_Web.html')
 
+@app.route('/fortune_wheel')
+def fortune_wheel():
+    return render_template('fortune_wheel.html')
 
-if __name__ == "__main__":
-    app.run(port='8000', host='127.0.0.1', debug=True)
+@app.route('/dice')
+def dice():
+    return render_template('dice.html')
+
+@app.route('/generator')
+def generator():
+    return render_template('generator.html')
+
+@app.route('/monetka')
+def monetka():
+    return render_template('monetka.html')
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
